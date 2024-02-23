68b90939c385a0489a456849bce76b4e27ac0fca
Francisco Gago <frangago71@gmail.com>
2024-02-23 10:24:00 +0100

    Lab1

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,12 +6,97 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
-      // TODO: Include the rest of the fields of the Restaurants table
+      },
 
+      // TODO: Include the rest of the fields of the Restaurants table
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      description: {
+        type: Sequelize.TEXT
+      },
+
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      url: {
+        type: Sequelize.STRING
+      },
+
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE 
+      },
+
+      averageServiceMinutes: {
+        type: Sequelize.DOUBLE 
+      },
+
+      email: {
+        type: Sequelize.STRING 
+      },
+
+      phone: {
+        type: Sequelize.STRING 
+      },
+
+      logo: {
+        type: Sequelize.STRING 
+      },
+
+      heroImage: {
+        type: Sequelize.STRING 
+      },
+
+      status: {
+        type: Sequelize.ENUM,
+        values:['online', 'offline', 'closed', 'temporarily closed'] 
+      },
+
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'User'
+          },
+          key: 'id'
+        }
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Restaurants')
   }
 }
+
